86c6ffe905c279f86d2faffbc80e1392851fc758
Salma Halhoul <halhoulsa00@gmail.com>
2024-07-24 21:29:39 +0000

    Fade in images with a placeholder(using transparent_image packing from pub.dev)

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,35 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 // statefulWidget = can call build more time to have interactivity
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-  // fields = attributes = instance variable = state
-  var count = 0;
-  @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              // call build
-              setState(() {
-                count++;
-              });
-            },
-            child: Text("you clicked $count times")
+      home: Stack(
+        children: [
+          Center(
+        child: CircularProgressIndicator(),
+      ), 
+      Center(
+        // use FadeInImage.assetNetwork for local images 
+        child: FadeInImage.memoryNetwork(//image be showed from opacity 0 to 1
+          placeholder: kTransparentImage,
+          image: 'https://s3-eu-west-1.amazonaws.com/blog-ecotree/blog/0001/01/ad46dbb447cd0e9a6aeecd64cc2bd332b0cbcb79.jpeg',
         ),
       ),
+        ],
+      )
     );
   }
+
 }
