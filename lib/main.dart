3651c7bc17b9a895b3ea275d8ce0c96bdd97a6bd
Salma Halhoul <halhoulsa00@gmail.com>
2024-07-24 20:17:06 +0000

    statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,39 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:simple_project/pages/login.dart';
-import 'package:simple_project/pages/register.dart';
-import 'package:simple_project/pages/welcome.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// statefulWidget = can call build more time to have interactivity
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner:
-          false, //false: remove debug ticket, true: show it
-      theme: ThemeData.light(useMaterial3: true),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Welcome(),
-        '/login': (context) => const Login(),
-        '/signup': (context) => const Register(),
-      },
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
+class MyAppState extends State<MyApp> {
+  // fields = attributes = instance variable = state
+  var count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              // call build
+              setState(() {
+                count++;
+              });
+            },
+            child: Text("you clicked $count times")
+        ),
+      ),
+    );
+  }
+}
