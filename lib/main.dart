2240ca71fc6d62e46f2402db0ca4d4da8ac98fd7
Salma Halhoul <halhoulsa00@gmail.com>
2024-07-25 20:29:24 +0000

    Tabs - add comments

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        length: 3,
+        length: 3,//number of tabs
         child: Scaffold(
           appBar: AppBar(
             title: Text("Tab Bar Demo"),
             centerTitle: true,
             bottom: TabBar(
               tabs: <Widget>[
-                Tab(icon: Icon(Icons.home), text: 'home',),
+                Tab(icon: Icon(Icons.home), text: 'home',),//tab title
                 Tab(icon: Icon(Icons.search), text: 'search',),
                 Tab(icon: Icon(Icons.person), text: 'profile',),
               ],
             ),
           ),
-          body: TabBarView(children: <Widget>[
+          body: TabBarView(children: <Widget>[//tab content
             Center(child: Text("Home Page")),
             Center(child: Text("Search Page")),
             Center(child: Text("Profile Page")),
