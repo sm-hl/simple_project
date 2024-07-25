48ef9f5b68ba3fa2ef08bc839006f6f84bb1a989
Salma Halhoul <halhoulsa00@gmail.com>
2024-07-25 20:26:19 +0000

    Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          // solutio2: using the builder widget
-          child: Builder(builder: (context) {
-            return ElevatedButton(
-              child: Text('Show SnackBar'),
-              onPressed: () {
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(SnackBar(content: Text('your message here')));
-              },
-            );
-          }),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Tab Bar Demo"),
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.home), text: 'home',),
+                Tab(icon: Icon(Icons.search), text: 'search',),
+                Tab(icon: Icon(Icons.person), text: 'profile',),
+              ],
+            ),
+          ),
+          body: TabBarView(children: <Widget>[
+            Center(child: Text("Home Page")),
+            Center(child: Text("Search Page")),
+            Center(child: Text("Profile Page")),
+          ]),
         ),
       ),
     );
