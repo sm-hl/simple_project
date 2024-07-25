617bd4d5564f85eecece6c4aa0623c4b6d158a92
Salma Halhoul <halhoulsa00@gmail.com>
2024-07-25 16:40:20 +0000

    solution1: SnackBar inside the scaffold build widget error and solutions

--- lib/main.dart
+++ lib/main.dart
@@ -6,99 +6,33 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Flutter App',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 0, 110, 124),
-          iconTheme: IconThemeData(color: Colors.white), //color drawer icon
+        body: Center(
+          child: MyButton(),
         ),
-        //menu sidebar ->in left, endDrawer: Drawer() -> in right
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 0, 110, 124),
-                ),
-                padding: EdgeInsets.all(12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundImage:
-                              AssetImage('assets/images/profil.jpg'),
-                        ),
-                        SizedBox(width: 10),
-                        Text('Rizky',
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.white)),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Text("Last activity : 12-02-2024 12:24 am",
-                        style: TextStyle(fontSize: 9, color: Colors.white))
-                  ],
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.home,
-                    size: 20, color: Color.fromARGB(255, 0, 110, 124)),
-                title: Text('Home',
-                    style: TextStyle(fontSize: 12, color: Colors.black)),
-                // subtitle: Text('Home', style: TextStyle(fontSize: 11, color: Colors.black)),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.person_3_rounded,
-                    size: 20, color: Color.fromARGB(255, 0, 110, 124)),
-                title: Text('Profile',
-                    style: TextStyle(fontSize: 12, color: Colors.black)),
-                // subtitle: Text('Home', style: TextStyle(fontSize: 11, color: Colors.black)),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.settings,
-                    size: 20, color: Color.fromARGB(255, 0, 110, 124)),
-                title: Text('Settings',
-                    style: TextStyle(fontSize: 12, color: Colors.black)),
-                // subtitle: Text('Home', style: TextStyle(fontSize: 11, color: Colors.black)),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.logout,
-                    size: 20, color: Color.fromARGB(255, 0, 110, 124)),
-                title: Text('Logout',
-                    style: TextStyle(fontSize: 12, color: Colors.black)),
-                // subtitle: Text('Home', style: TextStyle(fontSize: 11, color: Colors.black)),
-                onTap: () {
-                  print('logout');
-                },
-              ),
-            ],
-          ),
-        ),
-        // body: null,
       ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text('Show SnackBar'),
+      onPressed: () {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('your message here')));
+      },
     );
   }
 }
