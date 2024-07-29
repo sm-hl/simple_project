a82dadb9c15f9ff155a95348c5a5df3a1bc5f8c7
Salma Halhoul <halhoulsa00@gmail.com>
2024-07-29 12:31:56 +0000

    1-ListView : used for a small number of items but decrease performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,58 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
-import 'package:simple_project/pages/home.dart';
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.purple[700],
-        floatingActionButtonTheme: FloatingActionButtonThemeData(foregroundColor: Colors.purple[200], backgroundColor: Colors.purple[900]),
-        tabBarTheme: TabBarTheme(indicatorColor: Colors.purple[200], labelColor: Colors.purple[100], unselectedLabelColor: Colors.white38, dividerColor: Colors.purple[900]),
-        secondaryHeaderColor: Colors.red,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.purple[900], foregroundColor: Colors.white70),
-        scaffoldBackgroundColor: Colors.purple[200],
-        iconTheme: IconThemeData(size: 100,color: Colors.purple[900]),//for all icons in app (named by : app wide theme)
-      ),
       home: Home(),
     );
   }
 }
 
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('all about listView'),
+      ),
+      body: ListView(//used for a small number of items => decrease performance
+        children: [
+          Container(
+            color: Colors.red,
+            height: 200,
+            child: Text('box'),
+          ),
+          Container(
+            color: Colors.green,
+            height: 200,
+            child: Text('box'),
+          ),
+          Container(
+            color: Colors.blue,
+            height: 200,
+            child: Text('box'),
+          ),
+          Container(
+            color: Colors.grey,
+            height: 200,
+            child: Text('box'),
+          ),
+        ],
+      ),
+    );
+  }
+}
