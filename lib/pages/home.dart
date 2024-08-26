3a1633522b4bf342ca313c348fb00b651cf4e5bf
Salma Halhoul <halhoulsa00@gmail.com>
2024-08-26 16:49:56 +0000

    Forms Focus and text fields

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //tree steps to focus the next input
+  //1-create a focusNode
+  var myFocus;
+  @override
+  void initState() {
+    super.initState();
+    myFocus = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    myFocus.dispose();//to distroy focus node
+  }
+
   final formKey = GlobalKey<FormState>(); //is only for a single form
   @override
   Widget build(BuildContext context) {
@@ -17,35 +32,59 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Forms 1'),
       ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Enter your name', //placeholder
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true, // to make the cursor on this input
+                decoration: InputDecoration(
+                  hintText: 'first name', //placeholder
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "first name required";
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Name required";
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 22),
-            ElevatedButton(
-              child: Text('Send'),
-              onPressed: () {
-                //test if form is valid using a key specific for this form
-                if (formKey.currentState!.validate()) {
-                  //store data to local db or send it to api on server
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text('Success : Data has been sent!!')));
-                }
-              },
-            )
-          ],
+              SizedBox(height: 22),
+              TextFormField(
+                //2-pass the focusNode to a textField
+                focusNode: myFocus,
+                decoration: InputDecoration(
+                  hintText: 'last name', //placeholder
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "last name required";
+                  }
+                  return null;
+                },
+              ),
+              // ElevatedButton(
+              //   child: Text('Send'),
+              //   onPressed: () {
+              //     //test if form is valid using a key specific for this form
+              //     if (formKey.currentState!.validate()) {
+              //       //store data to local db or send it to api on server
+              //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              //           content: Text('Success : Data has been sent!!')));
+              //     }
+              //   },
+              // )
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //3-give focus to the textfield when button is tapped
+          myFocus.requestFocus();
+        },
+        child: Text('next'),
       ),
     );
   }
