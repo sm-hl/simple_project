adf03b9ec4e1dcf549a6a9e168f50a8e814c088e
Salma Halhoul <halhoulsa00@gmail.com>
2024-08-29 12:47:59 +0000

    Temperature Conversion demo1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,40 +8,79 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Color? selectedColor;
+  String tempValue = '';
+  String tempType = 'Celsius';
+  double result = 0.0;
+
+  double calculatTemperature() {
+    if (tempValue.isEmpty) {
+      return 0.0;
+    }
+    if (tempType == 'Celsius') {
+      return (double.parse(tempValue) * 9 / 5) + 32;
+    } else if (tempType == 'Fahrenheit') {
+      return (double.parse(tempValue) - 32) * 5 / 9;
+    } else {
+      return 0.0;
+    }
+  }
+
+  void handleConversion() {
+    setState(() {
+      result = calculatTemperature();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: selectedColor,
       appBar: AppBar(
-        title: Text('Switch'),
+        title: Text('Temperature Conversion demo1'),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Orange', style: TextStyle(fontSize: 20),),
-            Radio(
-              value: Colors.orange,//open=if value = groupeValue, close:verse
-              groupValue: selectedColor,
-              onChanged: (newValue) {
+            // input
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                  labelText: 'Enter Temp to convert to $tempType',
+                  hintText: 'Enter Temperature'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
                 setState(() {
-                  selectedColor = newValue;//newValue take the value of Radio
+                  tempValue = value;
                 });
               },
             ),
-            RadioListTile(//just have a label
-              title: Text('Blue'),
-              value: Colors.blue,//open=if value = groupeValue, close:verse
-              groupValue: selectedColor,
+            //radios
+            RadioListTile(
+              //just have a label
+              title: Text('ºF'),
+              value: 'Fahrenheit', //open=if value = groupeValue, close:verse
+              groupValue: tempType,
               onChanged: (newValue) {
                 setState(() {
-                  selectedColor = newValue;//newValue take the value of Radio
+                  tempType = newValue!; //newValue take the value of Radio
                 });
               },
             ),
+            RadioListTile(
+              //just have a label
+              title: Text('ºC'),
+              value: 'Celsius', //open=if value = groupeValue, close:verse
+              groupValue: tempType,
+              onChanged: (newValue) {
+                setState(() {
+                  tempType = newValue!; //newValue take the value of Radio
+                });
+              },
+            ),
+            // button
+            ElevatedButton(onPressed: handleConversion, child: Text('Convert')),
+            Text('Result : ${result.toStringAsFixed(2)}')
           ],
         ),
       ),
